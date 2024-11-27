4559caf1891f1123376a7b0ded1b1f5c9fd5723a
adanosn <adanosn@gmail.com>
2024-11-27 18:52:13 +0200

    Step 1.2.2 -RedLine constructor and properties. Green

--- src/SevenRedLinesCore/RedLine.cs
+++ src/SevenRedLinesCore/RedLine.cs
@@ -1,5 +1,9 @@
 ﻿namespace SevenRedLines.Core;
 
-public class RedLine
+public class RedLine(int x1, int y1, int x2, int y2)
 {
+    public int X1 { get; } = x1;
+    public int Y1 { get; } = y1;
+    public int X2 { get; } = x2;
+    public int Y2 { get; } = y2;
 }
--- src/Tests/SevenRedlines.Core.Tests/SevenRedLinesTests.cs
+++ src/Tests/SevenRedlines.Core.Tests/SevenRedLinesTests.cs
@@ -26,20 +26,11 @@ namespace SevenRedLines.Core.Tests
             var x2 = 30;
             var y2 = 40;
 
-
-            //This code might seem like it has a “bad smell,” and you may feel inclined to introduce additional classes,
-            //but all in due time.
-            //Imagine you know very little about geometry.
-            //For the purpose of learning TDD, let’s proceed in small steps.
-
             var redLine = new RedLine(x1, y1, x2, y2);
-
             Assert.Equal(x1, redLine.X1);
             Assert.Equal(y1, redLine.Y1);
             Assert.Equal(x2, redLine.X2);
             Assert.Equal(y2, redLine.Y2);
-
-            // Build failure
         }
     }
 }
